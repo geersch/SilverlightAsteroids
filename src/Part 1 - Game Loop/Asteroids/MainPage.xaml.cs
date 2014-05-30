b5c4765ed01dbc3f101f3b1a84581c23c56e5052
Christophe Geers <geersch@gmail.com>
2014-05-30 12:31:51 +0200

    Added the source code for Part 1 - Game Loop

---
+++ src/Part 1 - Game Loop/Asteroids/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Controls;
+using CGeers.Silverlight.GameEngine;
+
+namespace Asteroids
+{
+    public partial class MainPage : UserControl
+    {
+        private GameSurface _gameSurface = GameSurface.GetInstance();
+        private int _fps;
+        private DateTime _lastFpsReport;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this._gameSurface.RenderFrame += RenderFrame;
+            this._gameSurface.StartGame();
+        }
+
+        private void RenderFrame(object sender, RenderFrameEventArgs e)
+        {
+            // Calculate the frame rate
+            this._fps++;
+            if ((DateTime.Now - this._lastFpsReport).Seconds >= 1)
+            {
+                FrameRate.Text = String.Format("{0} FPS", this._fps);
+                this._fps = 0;
+                this._lastFpsReport = DateTime.Now;                
+            }
+        }
+    }
+}
